b3ccc5adaef44e892d5b960634ed914b61365b7e
Eduardo Díaz <iamedu@gmail.com>
2014-07-20 13:50:23 -0500

    Added exception handlers

--- grails-app/services/iamedu/music/AuthorizationLoginService.groovy
+++ grails-app/services/iamedu/music/AuthorizationLoginService.groovy
@@ -4,6 +4,9 @@ import grails.transaction.Transactional
 import org.apache.shiro.SecurityUtils
 import org.apache.shiro.authc.UsernamePasswordToken
 
+import org.apache.shiro.authc.UnknownAccountException
+import org.apache.shiro.authc.IncorrectCredentialsException
+
 import iamedu.raml.http.RamlResponse
 
 @Transactional
@@ -16,6 +19,28 @@ class AuthorizationLoginService {
     subject.login(token)
     RamlResponse.create().body([
       token: subject.session.id
+    ])
+  }
+
+  def handleInvalidUsername(UnknownAccountException ex) {
+    RamlResponse.create().statusCode(403).body([
+      hola: 'Usuario invalido',
+      message: ex.message
+    ])
+  }
+
+  // def handleInvalidPassword(IncorrectCredentialsException ex) {
+  //   RamlResponse.create().statusCode(403).body([
+  //     hola: 'Password invalido',
+  //     message: ex.message
+  //   ])
+  // }
+
+  def handleEverything(org.apache.shiro.authc.AuthenticationException ex) {
+    println ex
+    RamlResponse.create().statusCode(403).body([
+      hola: 'No se',
+      message: ex.message
     ])
   }
 
