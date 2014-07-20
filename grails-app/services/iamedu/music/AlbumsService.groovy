96a2fa0e4ad0444b147b237054e8841f38e37942
Eduardo Díaz <iamedu@gmail.com>
2014-07-20 08:53:44 -0500

    Using query param

--- grails-app/services/iamedu/music/AlbumsService.groovy
+++ grails-app/services/iamedu/music/AlbumsService.groovy
@@ -2,11 +2,13 @@ package iamedu.music
 
 import grails.transaction.Transactional
 import iamedu.raml.http.RamlResponse
+import iamedu.raml.http.RamlQueryParam
 
 @Transactional
 class AlbumsService {
 
-  RamlResponse post(Map data) {
+  RamlResponse post(@RamlQueryParam("access_token") String accessToken, Map data) {
+    data.accessToken = accessToken
     RamlResponse.create().body(data)
   }
 }
